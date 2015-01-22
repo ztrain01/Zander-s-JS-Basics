0125b6f94534d971416df9b8081a398a505d502c
Zander Allen <ztrain01@gmail.com>
2015-01-21 20:24:24 -0700

    3 problems completed

---
+++ practice.js
@@ -0,0 +1,88 @@
+//Once you complete a problem, open up Chrome and check the answer in the console.
+
+
+var name = 'Tyler';
+//Create a function called isTyler that accepts name as it's only argument.
+//If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+
+var isTyler = function(name) {
+	if(name === 'Tyler") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+//Next problem
+
+
+
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
+
+var getName = function() {
+  var name = prompt("Enter Name")
+  console.log(name)
+}
+
+
+//Next Problem
+
+
+
+//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+//then alerts "Welcome, " plus whatever the users name is.
+
+  //Code Here
+
+
+//Next problem
+
+
+
+
+//What is the difference between arguments and parameters?
+
+parameters are the variables, while the argument is the expression
+
+
+//Next problem
+
+
+
+//What are all the falsy values in JavaScript and how do you check if something is falsy?
+
+
+  //Answer Here
+
+
+
+//Next Problem
+
+
+
+//Create a function called myName that returns your name
+
+  //Code Here
+
+//Now save the function definition of myName into a new variable called newMyName
+
+  //Code Here
+
+//Now alert the result of invoking newMyName
+
+
+
+//Next problem
+
+
+
+//Create a function called outerFn which returns an anonymous function which returns your name.
+
+  //Code Here
+
+//Now save the result of invoking outerFn into a variable called innerFn.
+
+  //Code Here
+
+//Now invoke innerFn.
